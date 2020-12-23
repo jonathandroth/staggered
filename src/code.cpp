7b27b1d137332d8a6f9260b4ab30b4708cb60833
jonathanroth_MSFt <jonathanroth@g.harvard.edu>
2020-12-22 17:18:57 -0800

    Have added rcpp code for matrix mult and compile is working. Have not yet integrated the actual functions for multiplication

--- src/code.cpp
+++ src/code.cpp
@@ -1,2 +1,12 @@
 #include <Rcpp.h>
+#include <RcppEigen.h>
+// [[Rcpp::depends(RcppEigen)]]
+
 using namespace Rcpp;
+
+// [[Rcpp::export]]
+SEXP eigenMapMatMult(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
+  Eigen::MatrixXd C = A * B;
+
+  return Rcpp::wrap(C);
+}
