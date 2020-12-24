19924d87d0be50b04d444b2a876818d48ddf23c9
jonathanroth_MSFt <jonathanroth@g.harvard.edu>
2020-12-24 15:57:10 -0800

    Changed some NA defaults to NULL to avoid issues with vector NAs

--- R/compute_efficient_estimator_and_se.R
+++ R/compute_efficient_estimator_and_se.R
@@ -482,7 +482,7 @@ create_Atheta_list_for_simple_average_ATE <- function(g_list, t_list, N_g_list){
 #' @param beta Optional. A coefficient to use for covariate adjustment. If not specified, the plug-in optimal coefficient is used
 #' @param betaType. An optional parameter describing the type of covariate adjustment used. Defaults to "betaStar" if the efficient estimator is used.
 #' @return df A data.frame containing: thetahat (the point estimate), se (the standard error), se_conservative (the Neyman standard error), and betaType
-calculate_adjusted_estimator_and_se <- function(df, estimand =NULL, A_theta_list = NULL, A_0_list = NA, beta = NA, betaType = ifelse( is.na(beta), "betaStar", "Custom"), refine_S_g = T){
+calculate_adjusted_estimator_and_se <- function(df, estimand =NULL, A_theta_list = NULL, A_0_list = NULL, beta = NULL, betaType = ifelse( is.null(beta), "betaStar", "Custom"), refine_S_g = T){
 
   g_level_summaries <- compute_g_level_summaries(df, refine_S_g = refine_S_g)
   Ybar_g_list <- g_level_summaries$Ybar_g_List
@@ -505,14 +505,14 @@ calculate_adjusted_estimator_and_se <- function(df, estimand =NULL, A_theta_list
   if(is.null(A_theta_list)){stop("Estimand must be one of simple, cohort, or calendar; or custom A_theta_list must be provided")}
 
   #Create A_0_list if a custom A_0_list is not provided
-  if(is.na(A_0_list)){
+  if(is.null(A_0_list)){
     A_0_list <- create_A0_list(g_list = g_list, t_list = t_list)
   }
 
   Xvar_list <- purrr::pmap(.l = list(A_0_list, S_g_list, N_g_list) , .f = function(A0,S,N){ return(1/N * eigenMapMatMult( eigenMapMatMult(A0,S) , t(A0) ) ) } )
 #  Xvar_list <- purrr::pmap(.l = list(A_0_list, S_g_list, N_g_list) , .f = function(A0,S,N){ return(1/N * A0 %*%S %*% t(A0) )  } )
 
-  if(is.na(beta)){
+  if(is.null(beta)){
     beta <- compute_Betastar(Ybar_g_list, A_theta_list, A_0_list, S_g_list, N_g_list, Xvar_list = Xvar_list)
   }
 
